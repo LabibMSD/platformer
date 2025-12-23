b5f643332f8edbc379b12dfe00e80400539d0391
LabibMSD <labib.msd.3001@gmail.com>
2025-12-24 05:51:18 +0700

    Inisialisasi Project

---
+++ Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,20 @@
+﻿// 12/24/2025 AI-Tag
+// This was created with the help of Assistant, a Unity Artificial Intelligence product.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Object.FindFirstObjectByType<PlayerController>().gameObject;        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = true;
+    }
+}
---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO: Implement Running Mechanic in PlayerController
+
+- [ ] Add public float runSpeed = 12.0f; to PlayerController.cs
+- [ ] Modify Update method to calculate currentSpeed based on Shift key press
+- [ ] Update movement vector calculations to use currentSpeed instead of speed
+- [ ] Test the changes (run the game and verify running works with Shift)
